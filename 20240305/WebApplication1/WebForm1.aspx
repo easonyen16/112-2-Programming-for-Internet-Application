f14d1ac878bc5d24726579240e239b06c3a0eeed
easonyen16 <eason990024@gmail.com>
2024-03-05 12:07:15 +0800

    Add 0305 work

---
+++ 20240305/WebApplication1/WebForm1.aspx
@@ -0,0 +1,38 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="WebApplication1.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            font-size: xx-large;
+        }
+        #form1 {
+            text-align: center;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div style="text-align: center">
+            <span class="auto-style1">Ex0305 Array<br />
+            </span>
+            <br />
+            <asp:Button ID="Button1" runat="server" Height="108px" OnClick="Button1_Click" Text="Generate" Width="222px" />
+            <br />
+        </div>
+        <asp:Label ID="Label1" runat="server"></asp:Label>
+        <br />
+        <br />
+        <asp:Button ID="Button2" runat="server" Height="70px" OnClick="Button2_Click1" Text="Show" Width="158px" />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:Button ID="Button3" runat="server" Height="67px" OnClick="Button3_Click" Text="Reset" Width="152px" />
+        <br />
+        <br />
+        <asp:Label ID="Label2" runat="server"></asp:Label>
+    </form>
+</body>
+</html>
---
+++ 20240305/WebApplication1/WebForm1.aspx.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        static int[] data = new int[45];
+        Random rand=new Random();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            Label1.Text = "";
+            for (int i = 0; i < 45; i++)
+            {
+                data[i] = rand.Next(30, 100);
+            }
+            int counter = 0;
+            foreach (int k in data)
+            {
+                Label1.Text += k + " ";
+                counter++;
+                if (counter % 15 == 0)
+                {
+                    Label1.Text += "<br />";
+                }
+            }
+        }
+
+        protected void Button2_Click1(object sender, EventArgs e)
+        {
+            Array.Sort(data);
+            Array.Reverse(data);
+
+            int passMark = 60;
+            int passNo = 0;
+
+            for (int i = 0; i < data.Length; i++)
+            {
+                if (data[i] >= passMark)
+                {
+                    passNo++;
+                }
+            }
+
+            double passRatio = (double)passNo / data.Length * 100;
+            double average = data.Average();
+
+            int[] top3 = data.Take(3).ToArray();
+
+            string result = $"PassNo={passNo} Pass Ratio={passRatio:0}% <br />" +
+                            $"Average={average:0.0} <br />" +
+                            $"Top 3 >> {string.Join(", ", top3)}";
+
+            Label2.Text = result;
+        }
+
+
+        protected void Button3_Click(object sender, EventArgs e)
+        {
+            Label1.Text = "";
+            Label2.Text = "";
+        }
+    }
+}
